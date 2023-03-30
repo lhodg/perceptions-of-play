1033fcc664fec1af6032496e244545ae1c46e07f
lhodg <lucyhodgson@gmail.com>
2023-03-30 16:32:17 +0100

    Added clear hypotheses file and master data file. Added summary ANOVA

--- project_analysis.R
+++ project_analysis.R
@@ -25,23 +25,16 @@ edited_data %>%
   group_by(subject_role, play_type) %>%
   get_summary_stats(response, show = c("mean", "sd", "min", "max"))
 
-# ---------------------------------
+wide_data <- edited_data %>%
+  pivot_wider(
+    id_cols = c(subject_role,
+                subject_id,
+                play_type),
+    names_from = skill,
+    values_from = response
+  )
 
-# Hypothesis 1 & 2
-
-data_1a <- edited_data %>%
-  filter(skill != "academic")
-
-data_1a_parents <- data_1a %>%
-  filter(subject_role == "P")
-
-data_1a_teachers <- data_1a %>%
-  filter(subject_role == "T")
-
-data_3 <- edited_data %>%
-  list.filter (skill != "social") |
-
-?list.filter()
+# Summary ANOVA of all data
 
 anova_1a_parents <- anova_test(
   data = edited_data,
@@ -51,49 +44,169 @@ anova_1a_parents <- anova_test(
 )
 summary(anova_1a_parents)
 
-polr_1a_parents <- MASS::polr(
-  as.factor(response) ~ play_type,
-  data = data_1a_parents
+# Create composite score
+composite_data <- wide_data %>%
+  mutate(
+    composite = (social + emotional + communication) / 3
+  )
+
+# This form better for t-tests (above is better for plotting)
+composite_data_wide <- composite_data %>%
+  pivot_wider(
+    id_cols = c(subject_id, subject_role),
+    names_from = play_type,
+    values_from = social:composite
+  )
+
+# -------------------------
+# Hypothesis 1: both teachers and parents view pretend play as more
+# important for social/emotional/communication
+
+# Plot
+composite_data %>%
+  unite(
+    subject_and_play,
+    c(subject_role, play_type)
+  ) %>%
+  ggplot() +
+  geom_boxplot(
+    aes(
+      subject_and_play,
+      composite
+    )
+  )
+
+# tests for teachers ---------
+composite_data_teachers <- composite_data_wide %>%
+  filter(
+    subject_role == "T"
+  )
+
+# paired t-test (assumptions violated, but robust)
+t.test(
+  Pair(composite_pp, composite_ic) ~ 1,
+  data = composite_data_teachers,
+  alternative = "greater"
 )
-summary(polr_1a_parents)
 
-lm_1a_parents <- lm(response ~ 0 + play_type, data_1a_parents)
-summary(lm_1a_parents)
-
-get_anova_table(lm_1a_parents)
-
-
-anova_1a <- anova_test(
-  data = edited_data %>% filter(skill != "academic"),
-  dv = response,
-  wid = subject_id,
-  between = subject_role,
-  within = c(play_type, skill)
-
-  anova_1a <- anova_test(
-    data = edited_data %>% filter(skill != "academic"),
-    dv = response,
-    wid = subject_id,
-    between = subject_role,
-    within = c(play_type, skill)
-
+# fewer assumptions. Can't calculate exact p-value in this analysis
+wilcox.test(
+  Pair(composite_pp, composite_ic) ~ 1,
+  data = composite_data_teachers,
+  alternative = "greater"
 )
-get_anova_table(anova_1a)
 
+# same tests for parents ------
+composite_data_parents <- composite_data_wide %>%
+  filter(
+    subject_role == "P"
+  )
 
-  anova_1a <- anova_test(
-    data = edited_data %>% filter(subject_role != "T"),
-    dv = response,
-    wid = subject_id,
-    between = subject_role,
-    within = c(play_type, skill)
+t.test(
+  Pair(composite_pp, composite_ic) ~ 1,
+  data = composite_data_teachers,
+  alternative = "greater"
 )
-get_anova_table(anova_1a)
 
+# t.test(
+#   x = composite_data_teachers$composite_pp,
+#   y = composite_data_teachers$composite_ic,
+#   alternative = "greater",
+#   paired = TRUE
+# )
 
-ggplot(edited_data, aes(subject_role, response, fill=skill))+
-  stat_summary(fun=mean, geom="bar")+
-  stat_summary(fun.data=mean_cl_normal, geom="errorbar", width=0.2)+
-  facet_wrap(~ play_type)+
-  labs(x="Role", y="Mean rating")+
-  theme(legend.position="none")
+wilcox.test(
+  Pair(composite_pp, composite_ic) ~ 1,
+  data = composite_data_teachers,
+  alternative = "greater"
+)
+
+# ---------------------------
+# Hypothesis 2: teachers will view pretend play as more important for development
+# compared with parents
+
+composite_data %>%
+  filter(
+    play_type == "pp"
+  ) %>%
+  ggplot() +
+  geom_boxplot(
+    aes(
+      subject_role,
+      composite
+    )
+  )
+
+t.test(
+  composite_pp ~ subject_role,
+  data = composite_data_wide,
+  alternative = "less"
+)
+
+# t.test(
+#   composite_data_wide$composite_pp[composite_data_wide$subject_role == "P"],
+#   composite_data_wide$composite_pp[composite_data_wide$subject_role == "T"],
+#   alternative = "less"
+# )
+
+wilcox.test(
+  composite_pp ~ subject_role,
+  data = composite_data_wide,
+  alternative = "less"
+)
+
+# -------------------------
+# Hypothesis 3: no difference between parents and teachers in beliefs on
+# imaginary play for school readiness
+
+composite_data %>%
+  filter(
+    play_type == "ic"
+  ) %>%
+  ggplot() +
+  geom_boxplot(
+    aes(
+      subject_role,
+      academic
+    )
+  )
+
+t.test(
+  academic_ic ~ subject_role,
+  data = composite_data_wide,
+  alternative = "two.sided"
+)
+
+wilcox.test(
+  academic_ic ~ subject_role,
+  data = composite_data_wide,
+  alternative = "two.sided"
+)
+
+# ------------------------
+# Hypothesis 4: teachers likely to view pretend play as more important than
+# parents for academic readiness
+
+composite_data %>%
+  filter(
+    play_type == "pp"
+  ) %>%
+  ggplot() +
+  geom_boxplot(
+    aes(
+      subject_role,
+      academic
+    )
+  )
+
+t.test(
+  academic_pp ~ subject_role,
+  data = composite_data_wide,
+  alternative = "less"
+)
+
+wilcox.test(
+  academic_pp ~ subject_role,
+  data = composite_data_wide,
+  alternative = "less"
+)
